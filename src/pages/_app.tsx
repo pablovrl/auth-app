7d8f4e397e394187fce82483fed693456a1acd3f
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-20 01:34:30 -0300

    change default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/noto-sans";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Noto Sans",
+    body: "Noto Sans",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
