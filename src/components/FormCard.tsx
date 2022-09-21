6834868235f52697eb8370cdf4d8c45638938f21
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-21 12:15:52 -0300

    add create by text

--- src/components/FormCard.tsx
+++ src/components/FormCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container } from "@chakra-ui/react";
+import { Flex, Text, Box, Container } from "@chakra-ui/react";
 
 export const FormCard = ({ children }: { children: React.ReactNode }) => (
   <Container
@@ -9,14 +9,19 @@ export const FormCard = ({ children }: { children: React.ReactNode }) => (
     alignItems={{ md: "center" }}
     justifyContent="center"
   >
-    <Box
-      w={{ base: "full", md: "xl" }}
-      border={{ base: "0px", md: "2px" }}
-      p={{ base: 4, md: 10 }}
-      borderColor={{ md: "gray.100" }}
-      borderRadius="3xl"
-    >
-      {children}
-    </Box>
+    <Flex flexDirection={"column"}>
+      <Box
+        w={{ base: "full", md: "xl" }}
+        border={{ base: "0px", md: "2px" }}
+        p={{ base: 4, md: 10 }}
+        borderColor={{ md: "gray.100" }}
+        borderRadius="3xl"
+      >
+        {children}
+      </Box>
+      <Text mt={2} color={"gray.400"} fontSize="sm">
+        created by Pablo Villarroel ;)
+      </Text>
+    </Flex>
   </Container>
 );
